868d751d0bd6f8d5a6c62fb5d1428f989f69463b
Mateusz <mateuszdobrzanski@outlook.com>
2020-07-20 13:54:45 +0200

    create model class

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+# model class
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+#  gallery model
+class PostImage(models.Model):
+    post = models.ForeignKey(Post,
+                             default=None,
+                             on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.post.title
