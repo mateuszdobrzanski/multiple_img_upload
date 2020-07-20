7d429646c5bc47c1ccee48c87185d26209259408
Mateusz <mateuszdobrzanski@outlook.com>
2020-07-20 14:04:28 +0200

    add views methods

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, PostImage
 
-# Create your views here.
+
+def blog_view(request):
+    posts = Post.objects.all()
+    return render(request,
+                  'blog.html',
+                  {
+                      'posts': posts
+                  })
+
+
+def detail_view(request, id):
+    post = get_object_or_404(Post, id=id)
+    photos = PostImage.objects.first(post=post)
+    return render(request,
+                  'detail.html',
+                  {
+                      'post': post,
+                      'photos': photos,
+                  })
