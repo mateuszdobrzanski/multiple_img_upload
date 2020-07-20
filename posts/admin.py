e8e5cc09725df33c8cbb624255388e94939e18ce
Mateusz <mateuszdobrzanski@outlook.com>
2020-07-20 14:10:15 +0200

    register models in admin site, add base html files

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Post, PostImage
 
-# Register your models here.
+
+class PostImageAdmin(admin.StackedInline):
+    model = PostImage
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostImageAdmin]
+
+    class Meta:
+        model = Post
+
+
+@admin.register(PostImageAdmin)
+class PostImageAdmin(admin.ModelAdmin):
+    pass
